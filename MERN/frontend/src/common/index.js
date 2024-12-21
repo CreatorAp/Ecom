38da8337e7ce5b41db2e92092ae62727c359cdca
Arnon pedro <arnonpdr@gmail.com>
2024-12-21 13:32:55 -0300

    first commit

--- MERN/frontend/src/common/index.js
+++ MERN/frontend/src/common/index.js
@@ -1,4 +1,5 @@
-const backendDomain = "http://localhost:8080"
+// const backendDomain = "http://localhost:8080"
+const backendDomain = process.env.BACKEND_URL     
 
 const SummaryApi = {
     signUP : {
